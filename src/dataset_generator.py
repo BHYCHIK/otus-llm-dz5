b2017e4cf232bdb0ef8f95198e57f6341ac65779
Ivan Remen <bhyk@yandex.ru>
2026-02-03 10:37:34 +0300

    Make generator for arxiv dataset

---
+++ src/dataset_generator.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_category_page(link, category):
+    resp = requests.get(link)
+    if resp.status_code != 200:
+        raise Exception(f"Invalid status code {resp.status_code}")
+    soup = BeautifulSoup(resp.content, "html.parser")
+    articles = soup.find_all("div", class_="meta")
+    metadata = []
+    for article in articles:
+        title = article.find("div", class_="list-title").get_text()[6:].strip()
+        authors = article.find("div", class_="list-authors").get_text().split(',')
+        authors = [authors.strip() for authors in authors]
+        metadata.append({"title": title, "authors": authors})
+    pdf_links = soup.find_all("a", attrs={'title': 'Download PDF'})
+    i = 0
+    for pdf_link in pdf_links:
+        link = 'https://arxiv.org' + pdf_link["href"]
+        metadata[i]["link"] = link
+        i += 1
+    print(metadata)
+
+def main():
+    links_to_parse = []
+
+    resp = requests.get("https://arxiv.org/")
+    if resp.status_code != 200:
+        raise Exception(f"Invalid status code {resp.status_code}")
+    soup = BeautifulSoup(resp.content, "html.parser")
+    links = soup.find_all("a")
+    for link in links:
+        if link.has_attr("href") and link["href"].endswith("/new"):
+            href = 'https://arxiv.org' + link["href"]
+            cat = link.get("aria-labelledby").split(" ", 1)[1]
+            links_to_parse.append({'href': href, 'cat': cat})
+    for links_to_parse in links_to_parse:
+        get_category_page(links_to_parse['href'], links_to_parse['cat'])
+
+
+
+if __name__ == "__main__":
+    main()
--- src/rag/embedder.py
+++ src/rag/embedder.py
@@ -3,21 +3,24 @@ import torch
 
 class Embedder:
     device: str = "cpu"
-    embedding_model_name: str = "fitlemon/bge-m3-ru-ostap"
+    _embedding_model_name: str = "fitlemon/bge-m3-ru-ostap"
 
-    def __init__(self, device="cpu", model='fitlemon/bge-m3-ru-ostap'):
-        self.device = device
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        if device == "cuda":
+    def __init__(self, _device="cpu", model='fitlemon/bge-m3-ru-ostap'):
+        self.device = _device
+        _device = "cuda" if torch.cuda.is_available() else "cpu"
+        if _device == "cuda":
             print(f"   GPU: {torch.cuda.get_device_name(0)}")
 
-        self.embedding_model_name = model
+        self._embedding_model_name = model
 
-        self.embedding_model = HuggingFaceEmbeddings(
-            model_name=self.embedding_model_name,
+        self._embedding_model = HuggingFaceEmbeddings(
+            model_name=self._embedding_model_name,
             model_kwargs={'device': self.device},
             encode_kwargs={'normalize_embeddings': True}
         )
 
     def get_embedding(self, text: str) -> list[float]:
-        return self.embedding_model.embed_query(text=text)
+        return self._embedding_model.embed_query(text=text)
+
+    def get_model(self):
+        return self._embedding_model
