a3bc6adeafa9d5cb891e3843c6bf07a399049213
Ivan Remen <bhyk@yandex.ru>
2026-02-03 22:13:53 +0300

    Fix and config for hnsw

--- src/prepare_storage.py
+++ src/prepare_storage.py
@@ -4,7 +4,8 @@ from rag.vector_store import ChromaStore
 import time
 
 embedder = Embedder(model='BAAI/bge-m3')
-store = ChromaStore(embedder=embedder.get_model())
+#store = ChromaStore(embedder=embedder.get_model(), construction_ef=4, M=2, search_ef=1)
+store = ChromaStore(embedder=embedder.get_model(), construction_ef=100, M=16, search_ef=10)
 
 ITER_SIZE = 50
 
@@ -17,11 +18,11 @@ def generate_embeddings():
     splits_size = len(splits)
     i = 0
     while i < splits_size:
-        i += ITER_SIZE
         start = time.time()
         store.store_splits(splits[i:min(i+ITER_SIZE, splits_size)])
         end = time.time()
         print(f"Vectorized {i}. Chunk of {ITER_SIZE} vectorized for {end-start} seconds")
+        i += ITER_SIZE
     arxiv_dataset.clean()
     print("done")
 
--- src/rag/vector_store.py
+++ src/rag/vector_store.py
@@ -7,13 +7,19 @@ class VectorStore:
     def get_embedder(self):
         return self._embedder
 
-
+#TODO add configuration of HSNW and make it as name to dir
 class ChromaStore(VectorStore):
-    def __init__(self, embedder):
+    def __init__(self, embedder, space='cosine', construction_ef=100, M=16, search_ef=10):
         super().__init__(embedder=embedder)
         self._vector_store = Chroma(collection_name='arxiv',
-                                    persist_directory='../chroma/',
-                                    embedding_function=self.get_embedder())
+                                    persist_directory=f'../chroma_{space}_{construction_ef}_{M}_{search_ef}/',
+                                    embedding_function=self.get_embedder(),
+                                    collection_metadata={
+                                        'hnsw:space': space,
+                                        'hnsw:construction_ef': construction_ef,
+                                        'hnsw:M': M,
+                                        'hnsw:search_ef': search_ef
+                                    })
 
     def store_splits(self, splits):
         self._vector_store.add_documents(splits)
